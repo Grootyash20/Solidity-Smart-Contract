a82da26387ad9c82d5988f7eccaa4ea95b29c309
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-05-28 14:20:03 +0530

    Create DESIGNPAT-withdrawal.sol
    Design pattern

---
+++ DESIGNPAT-withdrawal.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract withdrawal{
+    address payable manager;
+    mapping(address=>uint) richestperson;
+    address payable richest;
+    uint richest_value;
+
+    constructor() payable{
+        richest = payable(msg.sender);
+        richest_value = msg.value;
+    }
+
+    function sendEther() public payable{
+        require(msg.value>richest_value,"You are not the richest guy outta here");
+        richest = payable(msg.sender);
+        richest_value = msg.value;
+        richestperson[msg.sender] = msg.value;
+    }
+
+    function withdraw() public payable{
+        require(richest==msg.sender,"What do you think you trynna do?");
+        uint amount = richestperson[msg.sender];
+        require(amount>0,"No funds to withdraw");
+        payable((msg.sender)).transfer(amount);
+        richestperson[msg.sender] = 0;
+        richest_value = 0;
+    }
+
+}
