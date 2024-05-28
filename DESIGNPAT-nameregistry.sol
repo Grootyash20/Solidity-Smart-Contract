62422861a9e29c226aa0822a43e5e1957e7c0b52
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-05-28 14:21:48 +0530

    Create DESIGNPAT-nameregistry.sol
    Design Pattern

---
+++ DESIGNPAT-nameregistry.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract test{
+    mapping(string=>ceo) CEO;
+    function createobj() public{
+    CEO["ceo"] = new ceo();
+    }
+}
+contract ceo{
+
+}
+contract hr{
+
+}
+contract employee{
+
+}
