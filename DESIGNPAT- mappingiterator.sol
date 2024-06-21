f715511b9e436f4689ee0e7d59ac60fc955c0191
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-06-21 22:11:31 +0530

    Update DESIGNPAT- mappingiterator.sol

--- DESIGNPAT- mappingiterator.sol
+++ DESIGNPAT- mappingiterator.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract test{ 
     mapping(address=>uint) map;  
-    address[] public arr;
+    address[] public arr; 
     function create() public{  
         //any code
         arr.push(msg.sender);//this is important as mapping is non iterable, so you can push it into an array.
